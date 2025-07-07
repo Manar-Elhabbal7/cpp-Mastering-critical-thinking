e118c288826a92117308e46a2f7fdee2adc9c4d3
Manar ELhabbal <manarelhabbal619@gmail.com>
2025-07-08 01:53:00 +0300

    Add files via upload

---
+++ Askme/colors.h
@@ -0,0 +1,8 @@
+#pragma once 
+#include <bits/stdc++.h>
+using namespace std;
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
