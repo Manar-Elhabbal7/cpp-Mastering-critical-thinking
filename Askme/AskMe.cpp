5c34355aa5f3037bbd15608326625eebd23bee84
Manar ELhabbal <manarelhabbal619@gmail.com>
2025-07-08 15:12:11 +0300

    Update AskMe.cpp

--- Askme/AskMe.cpp
+++ Askme/AskMe.cpp
@@ -7,7 +7,7 @@
 #include "system.h"
 using namespace std;
 
-
+//Author : The Goat 
 int main() {
     UserAuth auth;
     auth.Menu();
@@ -16,25 +16,7 @@ int main() {
         s.showMenu();
         s.choose();
     }
-        
+
     return 0;
 }
-//author : The GOAT
-/*
-the bugs untill now :
-    //1 : in function answer question it can be a thread or a question
-    //sol : can make another one for answer thread 
-    //solved by using a new file for just threads answers
 
-    //2: when i need to call show Menu can not as the implemention is not upove it
-    //sol : i can seperate all of classes in headers
-    //solve by the goat 
-
-    3: not done (funtion feed )
-
-    4: ask question i can ask thread (shown in pdf )
-
-    5.problem in function AskMe about UserAuth::status
-
-    6. in print all not print the answer of thread
-*/
